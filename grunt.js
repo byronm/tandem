3a102f545cea4aff1ec184a2d661846679818a2d
Jason Chen <jhchen7@gmail.com>
2013-01-29 15:56:05 -0800

    bump version, add socketio as dependency

--- grunt.js
+++ grunt.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     meta: {
-      version: '0.1.1',
+      version: '0.1.2',
       banner: 
         '// Tandem Realtime Coauthoring Engine - v<%= meta.version %> - ' +
           '//<%= grunt.template.today("yyyy-mm-dd") %>\n' +
