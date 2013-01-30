27cd698169c539907c131d9626af436b9a67a7e1
Jason Chen <jhchen7@gmail.com>
2013-01-29 16:43:37 -0800

    bump version

--- grunt.js
+++ grunt.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     meta: {
-      version: '0.1.2',
+      version: '0.1.3',
       banner: 
         '// Tandem Realtime Coauthoring Engine - v<%= meta.version %> - ' +
           '//<%= grunt.template.today("yyyy-mm-dd") %>\n' +
