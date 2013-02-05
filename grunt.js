16c4ff694aabafa58aaf964eb1aacd91f3a8e206
Jason Chen <jhchen7@gmail.com>
2013-02-04 18:44:32 -0800

    need to include build

--- grunt.js
+++ grunt.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     meta: {
-      version: '0.3.1',
+      version: '0.3.2',
       banner: 
         '// Tandem Realtime Coauthoring Engine - v<%= meta.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '// https://www.stypi.com/\n' +
