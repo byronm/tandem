5fc374f49ce3a984e454d57063bbada336e7931c
Jason Chen <jhchen7@gmail.com>
2013-08-07 11:34:17 -0700

    socketio in ie8 expects io to be global

--- src/ext/header.js
+++ src/ext/header.js
@@ -1,8 +1,7 @@
 (function() {
   var _ = window._.noConflict();
   var async = window.async.noConflict();
-  var io = window.io;
   var EventEmitter2 = window.EventEmitter2;
   var LinkedList = window.LinkedList;
-  window.async = window.io = window.LinkedList = window.EventEmitter2 = undefined;
+  window.async = window.LinkedList = window.EventEmitter2 = undefined;
   
