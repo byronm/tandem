9e44bded9d891b5695c270d271971c76da419c14
Jason Chen <jhchen7@gmail.com>
2014-01-21 16:31:31 -0800

    bump version

--- build/tandem.js
+++ build/tandem.js
@@ -1,4 +1,4 @@
-/*! Tandem Realtime Coauthoring Engine - v0.12.2 - 2014-01-21
+/*! Tandem Realtime Coauthoring Engine - v0.12.3 - 2014-01-21
  *  https://www.stypi.com/
  *  Copyright (c) 2014
  *  Jason Chen, Salesforce.com
